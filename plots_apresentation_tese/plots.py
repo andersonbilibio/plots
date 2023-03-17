8854a1da3698d421441670f110709a3eb906479c
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-17 15:53:35 -0300

    final

--- plots_apresentation_tese/plots.py
+++ plots_apresentation_tese/plots.py
@@ -10,47 +10,10 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 import cartopy.feature as cfeature
-from maps import plot_trajetorias
-from utils import circle_with_legend, config_labels, save
-from Ler_dados_raytracing_plots import  ler_dados_rt, forward_backward, raytrancing_parametros
+from src.base import load_files, plot_traj, plot_alt
 
-# from caracteristicas import plot2
-# from vento import plotTrajetorias
-# from utilis import circle_with_legend
-
-# import caracteristicas
-# import utilis 
-# import vento
-
-
- 
-    ###============================================================================
-    ###Topo---> para colocar color bar em função da ALTITUDE de parada
-    # ax = plt.subplot(G[0, :])
-    
-    # # The plot itself
-    # plt1 = ax.scatter(x, y, c = z, 
-    #                     marker = 's', s=20, edgecolor = 'none',alpha =1,
-    #                     cmap = 'magma_r', vmin =0 , vmax = 4)
-    
-    # cb = Colorbar(ax = ax, mappable = plt1, orientation = 'horizontal', ticklocation = 'top')
-    # cb.set_label(r'Colorbar !', labelpad=10)
-    
-    
-    ###============================================================================
-                    # N de linhas   # N de colunas
-    #map 0.1                #0:3
-    
-###============================================================================
-###--->Figura
-
-    ###============================================================================
-    ###---> Configurações da FIGUR
-    # caracteristicas.plot2()
-    # infos = f"$\lambda_H = ${lambdaH} km, $\\tau = $ {periodo} min, $c_H = $ {vel_fase} m/s, $\phi = $ {propagacao}°" 
-    #ax1.set_title(f"infos")
-    # ax1[0].text(0.2, 1.1, infos, transform = ax1[0].transAxes)
-
+import os
+from utils import circle_with_legend
 class MapaRT:
     
     
@@ -93,10 +56,15 @@ class MapaRT:
         g1.top_labels = False
         g1.right_labels = False
         
-        phase = "descendente"
-        circle_with_legend(mapa, [300, 700, 1100, 1500], scale = 110)
+        circle_with_legend(ax1, [300, 700, 1100, 1500], scale = 110)
         
-        plot_trajetorias(ax1, phase, rt_type = "forward")
+        infile = "database/vento/"
+
+        files = os.listdir(infile)
+        phase = "ascendente"
+        for filename in files:
+        
+            plot_traj(ax1, infile, filename, phase = "ascendente")
         
         return ax1
     
@@ -107,6 +75,16 @@ class MapaRT:
         ax3.set(ylim=self.mapa().get_ylim())
         ax3.set_xlabel('Altitude (km)')
         ax3.set_ylabel('Latitude (°)')
+        infile = "database/vento/"
+
+        files = os.listdir(infile)
+        phase = "ascendente"
+        for filename in files:
+            plot_alt(ax3, 
+                      infile, filename, 
+                      phase = "ascendente", 
+                      rt_type = "fordward", 
+                      alt_lon = False)
         return ax3
     
     def longitude(self):
@@ -114,14 +92,30 @@ class MapaRT:
         ax4.set(xlim=self.mapa().get_xlim())
         ax4.set_xlabel('Longitude (°)')
         ax4.set_ylabel('Altitude (km)')
+        infile = "database/vento/"
+
+        files = os.listdir(infile)
+        phase = "ascendente"
+        for filename in files:
+            plot_alt(ax4, 
+                      infile, filename, 
+                      phase = "ascendente", 
+                      rt_type = "fordward", 
+                      alt_lon = True)
         return ax4
     
 
     def tempo(self):
         ax5 = self.fig.add_subplot(self.G[3:4, 3:4])
-        ax5.set(ylim=self.longitude().get_ylim())
+        #ax5.set(ylim=self.longitude().get_ylim())
         #ax5.set_ylabel('Altitude (km)')
         ax5.set_xlabel('Horas (LT)')
+        import pandas as pd
+        df = pd.read_csv("ascendente_final_positions.txt", index_col = 0
+                         )
+        #df = df.mean()
+        ax5.grid()
+        ax5.scatter(df.time, df.alt, color = "b")
         return ax5
     
     
